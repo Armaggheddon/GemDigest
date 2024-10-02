2746605044a5a1b028c6027d4ae9f6c632bf9fb2
Armaggheddon <alebrum97@gmail.com>
2024-10-03 00:28:25 +0200

    Added exception handler, and refactored link filter

--- src/bot/gem_digest_bot.py
+++ src/bot/gem_digest_bot.py
@@ -1,13 +1,19 @@
 import asyncio
 
 from telebot.async_telebot import AsyncTeleBot
+from telebot import ExceptionHandler
+
+from configs import api_keys
 
 from .handlers import start_command
 from .handlers import link_message
-from configs import api_keys
+from . import filters
+from . import utils
 
+def register_custom_filters(bot: AsyncTeleBot) -> None:
+    bot.add_custom_filter(filters.LinkFilter())
 
-def register_handlers(bot: AsyncTeleBot):
+def register_handlers(bot: AsyncTeleBot) -> None:
     bot.register_message_handler(
         start_command.handle_start_command,
         commands=[start_command.CMD],
@@ -17,15 +23,19 @@ def register_handlers(bot: AsyncTeleBot):
     bot.register_message_handler(
         link_message.handle_link_message,
         content_types=['text'],
-        func=lambda msg: link_message.contains_url(msg.text),
+        func=lambda msg: utils.has_url(msg.text),
         pass_bot=True
     )
 
 
-def run():
+def run() -> None:
     
-    gem_digest_bot = AsyncTeleBot(token = api_keys.TELEGRAM_BOT_TOKEN)
+    gem_digest_bot = AsyncTeleBot(
+        token = api_keys.TELEGRAM_BOT_TOKEN, 
+        exception_handler=ExceptionHandler()
+    )
 
+    #register_custom_filters(gem_digest_bot)
     register_handlers(gem_digest_bot)
 
     asyncio.run(gem_digest_bot.polling())
