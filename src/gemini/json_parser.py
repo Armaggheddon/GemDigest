f9486d322a96a3f69949e48af3a6ba0a2da0fe00
Armaggheddon <alebrum97@gmail.com>
2024-10-05 22:13:45 +0200

    Refactor json_parser.py to handle gemini's janky JSON output, and added support for dynamic json object creation based on  optional `format` parameter

--- src/gemini/json_parser.py
+++ src/gemini/json_parser.py
@@ -1,35 +1,84 @@
+import io
+from typing import Mapping, Any, List
 from .types import GeminiOutputFormat
 
-output = """{"title": "LeafSense: AI-Powered Disease Detection for Vineyards", "summary": "🍇 LeafSense is a revolutionary app that uses AI to help vineyard owners identify diseases early on. Simply take a picture of a leaf with your smartphone, and LeafSense's algorithms will analyze it for signs of disease. The app provides a 'probability index' to let you know how likely a problem is. 💪 If you need more information, you can access a built-in encyclopedia of vineyard diseases or even export the image to your technician for further analysis. 🔍 The app also allows you to save results and locations to keep track of potential issues in your vineyard. 🗺️ LeafSense empowers farmers by offering real-time disease monitoring and helping them make informed decisions to protect their crops. 🌱", "image_url": "\\\"images/iphone-app-470.png\\\""}"""
 
-def parse_gemini_json(output: str) -> GeminiOutputFormat:
-    after_title = output.split("title", 1)[-1]
-    
-    tmp_vlue_list = []
-    tmp_vlue = ""
-    value_delimiter_count = 0
-    curr_pos = 0
-    for ch in after_title:
-        if value_delimiter_count == 2 or value_delimiter_count == 3:
-            if ch == "\"":
-                value_delimiter_count = 0
-                tmp_vlue_list.append(tmp_vlue)
-                if len(tmp_vlue_list) == 4:
-                    break
-                tmp_vlue = ""
-            tmp_vlue += ch 
-        if ch == "\"":
-            value_delimiter_count += 1
+def parse_gemini_json(
+    response_text: str, 
+    format: Mapping[str, Any]
+) -> GeminiOutputFormat:
+
+    expected_keys = list(format.__annotations__.keys())
+
+    # start from the first quote
+    start = response_text.find("\"")
+    curr_pos = start
+    delimiter_count = 0
+    tokens: List[str] = []
+
+    # split based on the quotes while keeping the quotes
+    while curr_pos < len(response_text):
+        if response_text[curr_pos] == "\"":
+            delimiter_count += 1
+
+        if delimiter_count == 2:
+            tokens.append(response_text[start:curr_pos + 1])
+            start = curr_pos + 1
+            delimiter_count = 0
+
         curr_pos += 1
+
+    # remove the quotes from the tokens only if are not escaped
+    for i, token in enumerate(tokens):
+        s_quote = token.find("\"")
+        e_quote = token.rfind("\"")
+
+        if s_quote == e_quote: # considers also the case where both are -1
+            # no quotes, or is only one quote (the same)
+            continue
+
+        s_quote_escaped = s_quote > 0 and token[s_quote - 1] == "\\"
+        e_quote_escaped = e_quote > 0 and token[e_quote - 1] == "\\"
+
+        if s_quote_escaped or e_quote_escaped:
+            # at least one quote is escaped
+            if token[s_quote - 1] == "\\":
+                # s_quote is escaped, so we remove the escape
+                s_quote = 0
+            else:
+                # s_quote is not escaped, so we add 1 to it
+                # to remove the first quote
+                s_quote = s_quote + 1
+            
+            if token[e_quote - 1] == "\\":
+                # e_quote is escaped, so we remove the escape
+                e_quote = len(token)
+            
+            tokens[i] = token[s_quote:e_quote].strip()
+        else:
+            # no escaped quotes
+            # add 1 to s_quote to remove the first quote
+            tokens[i] = token[s_quote+1: e_quote].strip()
     
-    missing = output[curr_pos:]
-    after_missing = missing.split("\"", 1)[-1]
-    after_missing = after_missing.rsplit("\"", 1)[0]
-    tmp_vlue_list.append(after_missing)
+    tmp_buff = io.StringIO()
+    values = []
+    
+    # accumulate the values for each of the keys
+    # the first token is the key, skip it so we can start
+    # accumulating from the first key value, to avoid
+    # haky code to remove the first item from values
+    for token in tokens[1:]:
+        if token in expected_keys:
+            # everything that has been found
+            # until now is the value of the previous key
+            values.append(tmp_buff.getvalue())
+            tmp_buff = io.StringIO()
+        else:
+            # we are still in the value of the current key
+            tmp_buff.write(token)
+    
+    # add the last value
+    values.append(tmp_buff.getvalue())
 
-    tmp_vlue_list.pop(1)
-    tmp_vlue_list.pop(2)
-
-    if len(tmp_vlue_list) != 3:
-        raise ValueError(f"Missing json Key: got {tmp_vlue_list}")
-    return GeminiOutputFormat(*tmp_vlue_list)
+    # TODO: return the values as a GeminiOutputFormat
+    return GeminiOutputFormat(*values)
