0d9560a04017d82892e5ef395a0e5e1b5c1174f9
Armaggheddon <alebrum97@gmail.com>
2024-10-05 22:14:32 +0200

    Refactor format_gemini_response to support dynamic json object creation based on optional `format` parameter

--- src/gemini/formatter.py
+++ src/gemini/formatter.py
@@ -1,17 +1,21 @@
 import logging
-from typing import Tuple
+from typing import Mapping, Any
 
 from configs.logger import setup_logger
 from .json_parser import parse_gemini_json
+from .types import GeminiOutputFormatTemplate
 
 setup_logger()
 logger = logging.getLogger(__name__)
 
 
-def format_gemini_response(raw_str: str) -> str:
+def format_gemini_response(
+    raw_str: str, 
+    format: Mapping[str, Any] = GeminiOutputFormatTemplate
+)-> str:
     """
     """
-    parserd_output = parse_gemini_json(raw_str)
+    parserd_output = parse_gemini_json(raw_str, format)
     
     title =  _title_builder(parserd_output.title)
     summary =  _summary_builder(parserd_output.summary)
