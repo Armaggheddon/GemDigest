60c2a4215f7e70c00e67f39dfdba1438f342e4bf
Armaggheddon <alebrum97@gmail.com>
2024-10-20 20:33:14 +0200

    Fixed bug where sequential calls to info would pass a string to sanitize_float throwing an exception

--- src/bot/handlers/info_command.py
+++ src/bot/handlers/info_command.py
@@ -47,21 +47,18 @@ async def handle_info_command(message: Message, bot: AsyncTeleBot) -> None:
 
     model_info: GeminiModelInfo = GeminiAPIClient.get_model_info()
 
-    model_info.model_name = markdown_v2_sanitizer.sanitize_code(
-        model_info.model_name
-        )
-    model_info.top_p = markdown_v2_sanitizer.sanitize_float(
-        model_info.top_p
-        )
-    model_info.temperature = markdown_v2_sanitizer.sanitize_float(
-        model_info.temperature
-        )
+    _model_name = markdown_v2_sanitizer.sanitize_code(
+        model_info.model_name)
+    _top_p = markdown_v2_sanitizer.sanitize_float(
+        model_info.top_p)
+    _temperature = markdown_v2_sanitizer.sanitize_float(
+        model_info.temperature)
     
     response_message = (
         "Here's the scoop on my current settings\\! 🧠✨\n\n"
-        f"\\- Model: `{model_info.model_name}` 🤖\n"
-        f"\\- Temperature: *{model_info.temperature}* 🌡️ \\(spiciness level\\!\\)\n"
-        f"\\- Top P: *{model_info.top_p}* 🎯 \\(sampling probability\\)\n"
+        f"\\- Model: `{_model_name}` 🤖\n"
+        f"\\- Temperature: *{_temperature}* 🌡️ \\(spiciness level\\!\\)\n"
+        f"\\- Top P: *{_top_p}* 🎯 \\(sampling probability\\)\n"
         f"\\- Top K: *{model_info.top_k}* 🎰 \\(number of options considered\\)\n"
         f"\\- Max output tokens: *{model_info.max_output_tokens}* 🚀 \\(how much I can say in one go\\!\\)\n\n"
         "Tuned and ready for action\\! ⚡"
