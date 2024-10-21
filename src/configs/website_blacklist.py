7cfb1030c5adab1e9b763d84b3dfd5d6870d62e7
Armaggheddon <alebrum97@gmail.com>
2024-10-21 20:32:18 +0200

    Added external website blacklist and update docker-compose files

---
+++ src/configs/website_blacklist.py
@@ -0,0 +1,20 @@
+
+def _get_website_blacklist():
+
+    _website_blacklist_path = "/gem_digest_bot/website_blacklist.txt"
+    blacklisted_websites = []
+
+    with open(_website_blacklist_path, "r") as f:
+        
+        for line in f:
+            line = line.strip()
+            if line.startswith("#") or not line:
+                continue
+            
+            blacklisted_websites.append(line)
+        
+    print(f"Blacklisted websites: {blacklisted_websites}")
+    return blacklisted_websites
+
+
+blacklisted_websites = _get_website_blacklist()
